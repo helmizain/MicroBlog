e5d8aa0caf2a986aabbaefd916b302428fbe96d3
muhfred <muhfred@outlook.com>
2016-06-25 13:20:56 +0200

    Add project files.

---
+++ MicroBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MicroBlog.Startup))]
+namespace MicroBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
